69c2842d6307b981ae16d9e1b8d05829d6f2635e
bilger-progress <bilger.yahov@progress.com>
2018-11-08 13:00:59 +0200

    Add new classes that implement the "ISQLite" interface.

---
+++ KinveyLiveServicesXamarin/KinveyLiveServicesXamarin.iOS/SQLite_iOS.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+using SQLite.Net.Platform.XamarinIOS;
+using Foundation;
+[assembly: Dependency(typeof(SQLite_iOS))]
+// ...
+public class SQLite_iOS : KinveyLiveServicesXamarin.ISQLite
+{
+    public SQLite_iOS() { }
+    public SQLite.Net.Interop.ISQLitePlatform GetConnection()
+    {
+        return new SQLitePlatformIOS();
+    }
+    public string GetPath()
+    {
+        return NSFileManager
+            .DefaultManager
+            .GetUrls(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomain.User)[0]
+            .ToString();
+    }
+}
